6511ec4bd33a0e3244b261955568c02cdb08a162
JS0108 <54683902+JS0108@users.noreply.github.com>
2021-07-03 21:49:03 +0800

    first commit

---
+++ easy/two_sum/twosum.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int cmpfcn(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    qsort(nums, numsSize, sizeof(int), cmpfcn);
+    for (int i = 0; i < numsSize; i++)
+        printf("%d ", nums[i]);
+    printf("\n");
+    *returnSize = 2;
+    int *arr = malloc(*returnSize * sizeof(int));
+    int i = 0;
+    int j = numsSize - 1;
+    
+    while (i < j) {
+        if (nums[i] + nums[j] > target) {
+            j--;
+        } else if (nums[i] + nums[j] < target) {
+            i++;
+        } else {
+            arr[0] = i;
+            arr[1] = j;
+            return arr;
+        }
+    }
+    
+    arr[0] = -1;
+    arr[1] = -1;
+    return arr;
+}
+
+int main() {
+    int nums[] = {3, 2, 4};
+    int returnSize;
+    int *arr = twoSum(nums, 3, 6, &returnSize);
+
+    for (int i = 0; i < 2;i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
